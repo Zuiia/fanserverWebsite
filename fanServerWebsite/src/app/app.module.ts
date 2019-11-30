f0f6b8e0650213c91a688b67a903aac1e0330753
jgonka <jonas.gonka@googlemail.com>
2019-11-30 01:30:27 +0100

    added new component

--- fanServerWebsite/src/app/app.module.ts
+++ fanServerWebsite/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
