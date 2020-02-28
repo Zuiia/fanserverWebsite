2c7dd2c2fb3b38c208258e05473bcfcd4541712c
jgonka <jonas.gonka@googlemail.com>
2020-02-28 19:15:14 +0100

    Made login actually work, added some buttons to navbar.component.html

--- fanServerServer/server.ts
+++ fanServerServer/server.ts
@@ -1,27 +1,31 @@
 import * as express from 'express';
 import * as passport from 'passport';
-import * as session from "express-session";
 import * as bodyParser from "body-parser";
+import * as mongo from 'mongodb';
 
+let session = require('express-session');
 let MemoryStore = require('memorystore')(session);
 
-const app = express();
+/**
+ * Setting up the database connection
+ */
+const dbClient = mongo.MongoClient;
+const dbUrl = "mongodb://localhost:27017";
+let dbo;
 
+dbClient.connect(dbUrl, function (err, db) {
+    if (err) console.error(err);
+
+    console.log("connected to database");
+    dbo = db.db("fanServerDB");
+});
+
+/**
+ * Setting up passport
+ */
 let DiscordStrategy = require('passport-discord').Strategy;
 
 let scopes = ['identify'];
-
-// TODO: implement saving of user
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-
-// TODO: implement retrieving of saved user
-passport.deserializeUser(function(id, done) {
-    // User.findById(id, function(err, user) {
-    //     done(err, user);
-    // });
-});
 
 passport.use(new DiscordStrategy({
     clientID: '427578630994919424',
@@ -34,6 +38,34 @@ passport.use(new DiscordStrategy({
     });
 }));
 
+passport.serializeUser(function(user, done) {
+    // @ts-ignore
+    user._id = user.id;
+
+    // @ts-ignore
+    dbo.collection("users").updateOne({"_id": user.id}, {$set: user}, {upsert: true}, function (err, res) {
+        if (err) done(err);
+
+        // @ts-ignore
+        done(null, user.id)
+    })
+});
+
+passport.deserializeUser(function(id, done) {
+    dbo.collection("users").findOne({"_id": id}, function (err, res) {
+        if (err) done(err);
+
+        if (res) {
+            done(null, res)
+        }
+    })
+});
+
+/**
+ * Setting up the server
+ */
+const app = express();
+
 app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 7 },
     store: new MemoryStore({
@@ -42,6 +74,9 @@ app.use(session({
     secret: 'keyboard cat'
 }));
 
+/**
+ * setting up Server Middleware
+ */
 app.use(express.static("public"));
 app.use(session({ secret: "lizisthecutestest" }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -50,22 +85,34 @@ app.use(passport.session());
 
 app.use('/', express.static(`${__dirname}/dist`));
 
-app.get('/login', function(req, res, next) {
-    passport.authenticate('discord', function(err, user, info) {
-        if (err) { return next(err); }
-        if (!user) { return res.redirect('/login'); }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.redirect('/users/' + user.username);
-        });
-    })(req, res, next);
+app.get('/login', passport.authenticate('discord', {scope: scopes, successRedirect: '/', failureRedirect:'/fail'}));
+
+// , function (req, res, next) {
+//     console.log("calling /login");
+//     passport.authenticate('discord', function(err, user, info) {
+//         if (err) { return next(err); }
+//         if (!user) { return res.redirect('/login'); }
+//         req.logIn(user, function(err) {
+//             if (err) { return next(err); }
+//             res.send(user);
+//             return res.redirect('/users/' + user.username);
+//         });
+//     })
+// });
+
+app.get('/login/callback',
+    passport.authenticate('discord', { failureRedirect: '/', successRedirect:'/info' })
+);
+
+app.get('/info', checkAuth, function(req, res) {
+    res.send(req.user);
 });
 
 app.use(function (req, res) {
     res.sendFile(`${__dirname}/dist/index.html`)
 });
 
-app.use(function (req, res, next) {
+app.use(function (req, res) {
     res.status(404).send("Sorry can't find that!")
 });
 
@@ -77,3 +124,8 @@ app.listen(80, function () {
     console.log('       Website :      http://localhost:80                    ');
     console.log('-------------------------------------------------------------');
 });
+
+function checkAuth(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.send('not logged in :(');
+}
