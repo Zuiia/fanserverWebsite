73e9031d6cfe91e635b25a18c67184dbcc335b50
Caja-Sophie Jakobs <caja-sophie.jakobs@gmx.de>
2020-02-28 17:40:44 +0100

    small routing fix

--- fanServerWebsite/src/app/app-routing.module.ts
+++ fanServerWebsite/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from "./app.component";
 import {TestimonialsComponent} from "./testimonials/testimonials.component";
+import {TestimonialPageComponent} from "./testimonial-page/testimonial-page.component";
 
 
 const routes: Routes = [
   {path: 'home', component: AppComponent},
-  {path: 'testimonials', component: TestimonialsComponent},
+  {path: 'testimonials', component: TestimonialPageComponent},
   {path: '**', redirectTo: '/'}
 ];
 
