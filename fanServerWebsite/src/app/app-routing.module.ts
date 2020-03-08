c90b391b3568566a5d2124311333c201b4b325b4
jgonka <jonas.gonka@googlemail.com>
2020-03-08 18:33:45 +0100

    Major style update to the homepage

--- fanServerWebsite/src/app/app-routing.module.ts
+++ fanServerWebsite/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled'})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
