ab65183c9930dd38669892d63fc5075420583dff
jgonka <jonas.gonka@googlemail.com>
2020-03-11 18:50:04 +0100

    More style update to the homepage and added content

--- fanServerWebsite/src/app/app-routing.module.ts
+++ fanServerWebsite/src/app/app-routing.module.ts
@@ -4,11 +4,17 @@ import {AppComponent} from "./app.component";
 import {TestimonialsComponent} from "./testimonials/testimonials.component";
 import {TestimonialPageComponent} from "./testimonial-page/testimonial-page.component";
 import {HomeComponent} from "./home/home.component";
+import {RulesPageComponent} from "./rules-page/rules-page.component";
+import {FaqPageComponent} from "./faq-page/faq-page.component";
+import {PatronPageComponent} from "./patron-page/patron-page.component";
 
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'testimonials', component: TestimonialPageComponent},
+  {path: 'rules', component: RulesPageComponent},
+  {path: 'faq', component: FaqPageComponent},
+  {path: 'patrons', component: PatronPageComponent},
   {path: '**', redirectTo: '/home'}
 ];
 
---
+++ fanServerWebsite/src/app/rules-page/rules-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rules-page',
+  templateUrl: './rules-page.component.html',
+  styleUrls: ['./rules-page.component.css']
+})
+export class RulesPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
